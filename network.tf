a5d2112e30024627017c346a2b880d8e894cf61f
milind69 <kulkarni.milindp@gmail.com>
2026-03-12 16:09:20 -0500

    adding cloud router

--- network.tf
+++ network.tf
@@ -32,8 +32,44 @@ resource "google_network_connectivity_hub" "ncc_hub" {
 resource "google_network_connectivity_spoke" "hybrid_spoke" {
     name    = "hybrid-spoke"
     hub     = google_network_connectivity_hub.ncc_hub.id  
-    location = var.region  
+    location = "global"  
     linked_vpc_network {
-      uri =google_compute_network.hybrid-vpc.id
+      uri =google_compute_network.hybrid-vpc.self_link
     }
+}
+
+resource "google_network_connectivity_spoke" "hybrid_service_spoke" {
+    name    = "hybrid-service-spoke"
+    hub     = google_network_connectivity_hub.ncc_hub.id  
+    location = "global"  
+    linked_vpc_network {
+      uri =google_compute_network.hybrid-service-vpc.self_link
+    }
+}
+
+
+resource "google_compute_router" "hybrid-router" {
+  name    = "hybrid-router"
+  network = google_compute_network.hybrid-vpc.id
+  region  = var.region
+}
+
+resource "google_compute_router_route_policy" "prepend_policy" {
+  name    = "prepend-east1"
+  router = google_compute_router.hybrid-router.name 
+  region  = var.region
+  terms {
+    priority = 100
+    match {
+      ip_prefix {
+        prefix = "10.2.2.0/24"
+        mask_length_range = "24-32"
+      }
+    }
+    action {
+      as_path_prepend {
+        as_paths = ["65001"]
+      }
+    }
+  }
 }
