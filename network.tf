07cc93496bea3dca0499bc57435ee6431fa00b52
milind69 <kulkarni.milindp@gmail.com>
2026-03-12 11:45:21 -0500

    initial commit

--- network.tf
+++ network.tf
@@ -10,6 +10,16 @@ resource "google_compute_subnetwork" "hybrid-subnet" {
   ip_cidr_range = "10.1.1.0/24"
   region = var.region
   network      = google_compute_network.hybrid-vpc.id
+}
 
-   z
+resource "google_compute_network" "hybrid-service-vpc" {
+  name                    = "hybrid-service-vpc"
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "hybrid-service-subnet" {
+  name          = "hybrid-service-subnet"
+  ip_cidr_range = "10.1.2.0/24"
+  region = var.region
+  network      = google_compute_network.hybrid-service-vpc.id
 }
