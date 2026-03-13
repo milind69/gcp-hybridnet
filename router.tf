72273989a291994c10181b1f9f26469f507d62e2
milind69 <kulkarni.milindp@gmail.com>
2026-03-12 21:59:04 -0500

    added router interfaces and peerinfg

--- router.tf
+++ router.tf
@@ -42,3 +42,68 @@ resource "google_compute_router_route_policy" "prepend_on_prefix_match" {
   #   }
   # }
 }
+
+# Interconnect and Peering 
+
+# #Ashburn IC attachment  - Note can not be done till interconnect is created for dedicated 
+# resource "google_compute_interconnect_attachment" "ashburn_ica" {
+#   name                     = "ashburn-ic"
+#   region                   = var.region
+#   type                     = "DEDICATED"
+#   router                   = google_compute_router.hybrid-router.name
+#   edge_availability_domain = "AVAILABILITY_DOMAIN_1"
+#   bandwidth                = "BPS_10G"
+
+# }
+
+# #Chicago IC
+# resource "google_compute_interconnect_attachment" "chicago_ica" {
+#   name                     = "chicago-ic"
+#   region                   = var.region
+#   type                     = "DEDICATED"
+#   router                   = google_compute_router.hybrid-router.name
+#   edge_availability_domain = "AVAILABILITY_DOMAIN_2"
+#   bandwidth                = "BPS_10G"
+
+# }
+# # Router Interfaces 
+
+# resource "google_compute_router_interface" "router-ashburn-interface-01" {
+#   name                    = "router-ashburn-interface-01"
+#   router                  = google_compute_router.hybrid-router.name
+#   region                  = var.region
+#   ip_range                = "169.254.10.1/30"
+#   interconnect_attachment = google_compute_interconnect_attachment.ashburn_ic.name
+# }
+
+# resource "google_compute_router_interface" "router-chicago-interface-01" {
+#   name                    = "router-chicago-interface-01"
+#   router                  = google_compute_router.hybrid-router.name
+#   region                  = var.region
+#   ip_range                = "169.254.10.1/30"
+#   interconnect_attachment = google_compute_interconnect_attachment.chicago_ic.name
+# }
+
+# #BGP Peering 
+
+# resource "google_compute_router_peer" "ashburn-peer" {
+#   name            = "ashburn-peer"
+#   router          = google_compute_router.hybrid-router.name
+#   region          = var.region
+#   peer_asn        = var.peer_asn
+#   interface       = google_compute_router_interface.router-ashburn-interface-01.name
+#   peer_ip_address = "169.254.10.2"
+#   export_policies = [google_compute_router_route_policy.prepend_on_prefix_match.name]
+
+# }
+
+# resource "google_compute_router_peer" "chicago-peer" {
+#   name            = "chicago-peer"
+#   router          = google_compute_router.hybrid-router.name
+#   region          = var.region
+#   peer_asn        = var.peer_asn
+#   interface       = google_compute_router_interface.router-chicago-interface-01.name
+#   peer_ip_address = "169.254.20.2"
+#   export_policies = [google_compute_router_route_policy.prepend_on_prefix_match.name]
+# }
+
