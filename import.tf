229355ba539b918672d60856d1004bf764bd80e4
milind69 <kulkarni.milindp@gmail.com>
2026-03-12 21:08:46 -0500

    added router policy

---
+++ import.tf
@@ -0,0 +1,34 @@
+# import {
+#   to = google_compute_network.hybrid-vpc
+#   id = "projects/networking-485219/global/networks/hybrid-spoke-vpc"
+# }
+
+# import {
+#   to = google_compute_network.hybrid-service-vpc
+#   id = "projects/networking-485219/global/networks/hybrid-service-vpc"
+# }
+
+# import {
+#   to = google_network_connectivity_hub.ncc_hub
+#   id = "projects/networking-485219/locations/global/hubs/ncc-hub"
+# }
+
+# import {
+#   to = google_compute_subnetwork.hybrid-subnet
+#   id = "projects/networking-485219/regions/us-central1/subnetworks/hybrid-spoke-subnet"
+# }
+
+# import {
+#   to = google_compute_subnetwork.hybrid-service-subnet
+#   id = "projects/networking-485219/regions/us-central1/subnetworks/hybrid-service-subnet"
+# }
+
+# import {
+#   to = google_network_connectivity_spoke.hybrid_spoke
+#   id = "projects/networking-485219/locations/global/spokes/hybrid-spoke"
+# }
+
+# import {
+#   to = google_network_connectivity_spoke.hybrid_service_spoke
+#   id = "projects/networking-485219/locations/global/spokes/hybrid-service-spoke"
+# }
--- network.tf
+++ network.tf
@@ -47,29 +47,45 @@ resource "google_network_connectivity_spoke" "hybrid_service_spoke" {
   }
 }
 
+#https://docs.cloud.google.com/network-connectivity/docs/router/concepts/overview
+#https://docs.cloud.google.com/network-connectivity/docs/router/concepts/bgp-route-policies-overview
 
 resource "google_compute_router" "hybrid-router" {
   name    = "hybrid-router"
   network = google_compute_network.hybrid-vpc.id
   region  = var.region
+  bgp {
+    asn = var.router_asn
+  }
 }
 
-# resource "google_compute_router_route_policy" "prepend_policy" {
-#   name    = "prepend-east1"
-#   router = google_compute_router.hybrid-router.name 
-#   region  = var.region
-#   terms {
-#     priority = 100
-#     match {
-#       ip_prefix {
-#         prefix = "10.2.2.0/24"
-#         mask_length_range = "24-32"
-#       }
-#     }
-#     action {
-#       as_path_prepend {
-#         as_paths = ["65001"]
-#       }
-#     }
-#   }
-# }
+resource "google_compute_router_route_policy" "prepend_on_prefix_match" {
+  router = google_compute_router.hybrid-router.name
+  region = var.region
+  name   = "prepend-on-prefix-match"
+  type   = "ROUTE_POLICY_TYPE_EXPORT"
+
+  terms {
+    priority = 100
+
+    match {
+      expression = "destination == '10.2.2.0/24'"
+    }
+
+    actions {
+      expression = "asPath.prependSequence([64620, 64620, 64620, 64620, 64620])"
+    }
+  }
+
+  # terms {
+  #   priority = 200
+
+  #   match {
+  #     expression = true
+  #   }
+
+  #   actions {
+  #     expression = "accept()"
+  #   }
+  # }
+}
